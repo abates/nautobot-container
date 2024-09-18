849152afd467af2ca2e1b160cca258a209444438
Andrew Bates <abates@omeganetserv.com>
2024-09-18 11:23:59 -0400

    Fixed nautobot_config.py

--- nautobot_config.py
+++ nautobot_config.py
@@ -141,51 +141,10 @@ PLUGINS = [
     "nautobot_ssot",
     "nautobot_ssot_unifi",
     "nautobot_design_builder",
-    "nautobot_plugin_nornir",
-    "nautobot_golden_config",
-    "nautobot_device_lifecycle_mgmt",
-    "nautobot_device_onboarding",
 ]
 
 # Plugins configuration settings. These settings are used by various plugins that the user may have installed.
 # Each key in the dictionary is the name of an installed plugin and its value is a dictionary of settings.
 PLUGINS_CONFIG = {
-    "nautobot_plugin_nornir": {
-        # "use_config_context": {"secrets": True}, 
-        "nornir_settings": {
-            "credentials": "nautobot_plugin_nornir.plugins.credentials.nautobot_secrets.CredentialsNautobotSecrets",
-            "runner": {
-                "plugin": "threaded",
-                "options": {
-                    "num_workers": 20,
-                },
-            },
-        }
-    },
-    "nautobot_golden_config": {
-        "per_feature_bar_width": 0.15,
-        "per_feature_width": 13,
-        "per_feature_height": 4,
-        "enable_backup": True,
-        "enable_compliance": True,
-        "enable_intended": True,
-        "enable_sotagg": True,
-        "enable_plan": True,
-        "enable_deploy": True,
-        "enable_postprocessing": False,
-        "sot_agg_transposer": None,
-        "postprocessing_callables": [],
-        "postprocessing_subscribed": [],
-        "jinja_env": {
-            "undefined": "jinja2.StrictUndefined",
-            "trim_blocks": True,
-            "lstrip_blocks": False,
-        },
-    },
-    "nautobot_device_lifecycle_mgmt": {
-        "barchart_bar_width": float(os.environ.get("BARCHART_BAR_WIDTH", 0.1)),
-        "barchart_width": int(os.environ.get("BARCHART_WIDTH", 12)),
-        "barchart_height": int(os.environ.get("BARCHART_HEIGHT", 5)),
-    },
 }
 
